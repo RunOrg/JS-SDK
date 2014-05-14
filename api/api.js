81af64fe42788646b5340dddc69ce7da0c05ad61
Victor Nicollet <victor@nicollet.net>
2014-05-14 16:20:57 +0200

    Group API start

--- api/api.js
+++ api/api.js
@@ -7,9 +7,9 @@ module.exports.Person = {
 	    'pic' 
 	],
 	statics: {
-	    Get: [ ['id'], [ 'GET', '/people/:id' ], 'Person' ]
+	    Get: [ ['id'], [ 'GET', '/people/:id' ], 'Person' ],
 	},
-	methods: {
+	methods: {	    
 	}
     },
     
@@ -23,10 +23,27 @@ module.exports.Person = {
 		'familyName' 
 	    ],
 	    statics: {
-		Get: [ ['id'], [ 'GET', '/people/:id/info' ], 'Person.Profile' ]
+		Get: [ ['id'], [ 'GET', '/people/:id/info' ], 'Person.Profile' ],
 	    },
 	    methods: {
 	    }
 	}
     }
+};
+
+module.exports.Group = {
+    
+    model: {
+	fields: [
+	    'id',
+	    'label'
+	],
+	statics: {
+	    Get: [ ['id'], [ 'GET', '/groups/:id' ], 'Group' ],
+	},
+	methods: {
+	    Create: [ [], [ 'POST', '/groups', { id: '@id?', label: '@label?' } ], 'this' ],		      
+	}
+    }
+
 };
